32b701c39b170df94c8b81946765f89a4531bee5
Alexander Franke <i02401253@emea.endress.com>
2020-08-03 13:22:06 +0200

    debugging some syntax errors

--- app.py
+++ app.py
@@ -41,5 +41,6 @@ def webhook():
         })
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000)
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
